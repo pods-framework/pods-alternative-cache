18ef311dd573f6ba06e83bcf0999082c8ed2af0f
Scott Kingsley Clark <lol@scottkclark.com>
2016-07-13 13:25:54 -0500

    Add object cache testing when using PODS_ALT_CACHE_DEBUG and ?altcache_debug=1

--- pods-alternative-cache.php
+++ pods-alternative-cache.php
@@ -49,3 +49,123 @@ function pods_alternative_cache_init() {
 
 }
 add_action( 'plugins_loaded', 'pods_alternative_cache_init' );
+
+/**
+ * Add support for cache debugging (to confirm writes are working as expected)
+ */
+function pods_alternative_cache_test_anon() {
+
+	if ( defined( 'PODS_ALT_CACHE_DEBUG' ) && PODS_ALT_CACHE_DEBUG && ! empty( $_GET['altcache_debug'] ) ) {
+		echo '<pre>';
+
+		$rand = (int) time();
+
+		$cache_key = 'pods-alt-cache-test';
+		$cache_group = 'pods-alt-cache';
+
+		$stats = array(
+			'rand'               => $rand,
+			'pods-alt-cache'     => array(
+				'before_set' => null,
+				'set'        => null,
+				'after_set'  => null,
+				'pass'       => false,
+			),
+			'wp-object-cache'    => array(
+				'before_set' => null,
+				'set'        => null,
+				'after_set'  => null,
+				'pass'       => false,
+			),
+			'wp-transient-cache' => array(
+				'before_set' => null,
+				'set'        => null,
+				'after_set'  => null,
+				'pass'       => false,
+			),
+		);
+
+		/**
+		 * Test Pods Alt Cache
+		 */
+
+		$cache_type = 'pods-alt-cache';
+		$cache_name = 'Pods Alt Cache';
+
+		$stats[ $cache_type ]['before_set'] = (int) pods_cache_get( $cache_key, $cache_group );
+
+		$stats[ $cache_type ]['set'] = pods_cache_set( $cache_key, $stats['rand'], $cache_group, 300 );
+
+		sleep( 1 );
+
+		$stats[ $cache_type ]['after_set'] = (int) pods_cache_get( $cache_key, $cache_group );
+
+		if ( $stats['rand'] == $stats[ $cache_type ]['after_set'] ) {
+			$stats[ $cache_type ]['pass'] = true;
+
+			var_dump( $cache_name . ' worked!' );
+		} else {
+			$stats[ $cache_type ]['pass'] = false;
+
+			var_dump( $cache_name . ' failed!' );
+		}
+
+		/**
+		 * Test WP Object Cache
+		 */
+
+		$cache_type = 'wp-object-cache';
+		$cache_name = 'WP Object Cache';
+
+		$stats[ $cache_type ]['before_set'] = (int) wp_cache_get( $cache_key, $cache_group );
+
+		$stats[ $cache_type ]['set'] = wp_cache_set( $cache_key, $stats['rand'], $cache_group, 300 );
+
+		sleep( 1 );
+
+		$stats[ $cache_type ]['after_set'] = (int) wp_cache_get( $cache_key, $cache_group );
+
+		if ( $stats['rand'] == $stats[ $cache_type ]['after_set'] ) {
+			$stats[ $cache_type ]['pass'] = true;
+
+			var_dump( $cache_name . ' worked!' );
+		} else {
+			$stats[ $cache_type ]['pass'] = false;
+
+			var_dump( $cache_name . ' failed!' );
+		}
+
+		/**
+		 * Test WP Transient Cache
+		 */
+
+		$cache_type = 'wp-transient-cache';
+		$cache_name = 'WP Transient Cache';
+
+		$stats[ $cache_type ]['before_set'] = (int) get_transient( $cache_key );
+
+		$stats[ $cache_type ]['set'] = set_transient( $cache_key, $stats['rand'], 300 );
+
+		sleep( 1 );
+
+		$stats[ $cache_type ]['after_set'] = (int) get_transient( $cache_key );
+
+		if ( $stats['rand'] == $stats[ $cache_type ]['after_set'] ) {
+			$stats[ $cache_type ]['pass'] = true;
+
+			var_dump( $cache_name . ' worked!' );
+		} else {
+			$stats[ $cache_type ]['pass'] = false;
+
+			var_dump( $cache_name . ' failed!' );
+		}
+
+		var_dump( $stats );
+
+		echo '</pre>';
+
+		die();
+	}
+
+}
+add_action( 'init', 'pods_alternative_cache_test_anon' );
